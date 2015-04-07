9dad3bc65347be6c9653d317aa59451763a860b4
Alex <spiridoncha@gmail.com>
2015-04-07 11:26:15 +0300

    add no copy obj of Buffer

--- Buffer/Buffer.h
+++ Buffer/Buffer.h
@@ -8,6 +8,7 @@ class Buffer
 	unsigned int real_size;
 	unsigned int size;
 	void re_size();
+	Buffer(const Buffer&) {}
 public:
 	Buffer(unsigned int s = 4) : real_size(0), size(s) { buf = new char[s]; }
 	~Buffer() { delete[] buf; }
