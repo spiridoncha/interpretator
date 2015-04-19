8e95d784870b51c919c5e9a20bcecc930a19fdba
Alex <spiridoncha@gmail.com>
2015-04-19 15:29:23 +0300

    add description of Parser

---
+++ Parser/Parser.cpp
@@ -0,0 +1,15 @@
+#include "Parser.h"
+#include <iostream>
+
+void Parser::analize()
+{
+	get_lex();
+	Program();
+	//prog.print()
+	std::cout << "ya" << std::endl;
+}
+
+void Parser::Program()
+{
+	/////
+}
--- Parser/Parser.h
+++ Parser/Parser.h
@@ -1,14 +1,26 @@
 #ifndef PARSER_H
 #define PARSER_H
-#include "../Scanner.Scanner.h"
+#include "../Scanner/Scanner.h"
 class Parser
 {
-
+	//Poliz prog;
+	explicit Parser(const char *program) : scan(program) {};
+	void get_lex() 
+	{
+		current_lex = scan.get_lex();
+		current_type_of_lex = current_lex.get_type();
+		current_value_int_of_lex = current_lex.get_value_int();
+		current_value_str_of_lex = current_lex.get_value_str();
+	}
+	void analize();
+	void Program();
+	void Description();
 private:
 	Scanner scan;
 	Lex current_lex;
 	type_of_lex current_type_of_lex;
-	int current_value_of_lex;
+	int current_value_int_of_lex;
+	String current_value_str_of_lex;
 	//Stack<int> st_int;
 	//Stack<type_of_lex> st_lex;
 };
