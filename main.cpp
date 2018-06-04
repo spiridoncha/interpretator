7e9a5f67cd9d4ee4f43b520902f1182bb29cf773
Spiridonov Alexander <spiridoncha@gmail.com>
2018-06-05 00:53:58 +0300

    Apply clang-format to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
 #include "Interpretator/Interpretator.h"
+#include <iostream>
 using namespace std;
-int main(int argc, char *argv[])
-{
-	try
-	{
-		if (argc != 2)
-		{
-			throw Arguments_Error(argc);
-		}
-		Interpretator inter(argv[1]);
-		inter.interpretation();
-	}
-	catch (const Except &a)
-	{
-		cout << a.what() << endl;
-	}
-	return 0;
+int main(int argc, char *argv[]) {
+  try {
+    if (argc != 2) {
+      throw Arguments_Error(argc);
+    }
+    Interpretator inter(argv[1]);
+    inter.interpretation();
+  } catch (const Except &a) {
+    cout << a.what() << endl;
+  }
+  return 0;
 }
